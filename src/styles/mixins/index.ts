4bcd53946c37395c120da2aaf367b95d042157f2
Haekang Jeon <1003gorkd@naver.com>
2023-04-10 14:02:35 +0900

    feat: setColor 함수 생성

--- src/styles/mixins/index.ts
+++ src/styles/mixins/index.ts
@@ -1,1 +1,5 @@
+export * from "./background";
+export * from "./border";
+export * from "./common";
+export * from "./size";
 export * from "./typography";
--- src/styles/mixins/typography/typography.ts
+++ src/styles/mixins/typography/typography.ts
@@ -1,4 +1,5 @@
 import { css } from "@emotion/react";
+import { CSSSelectorOptions, getSelectorStyle } from "../common";
 import { fontSize, fontWeight, lineHeight } from "./typography.constants";
 
 export const setTypography = (typography: keyof typeof fontSize) => {
@@ -6,5 +7,16 @@ export const setTypography = (typography: keyof typeof fontSize) => {
     font-size: ${fontSize[typography]}px;
     font-weight: ${fontWeight[typography]}px;
     line-height: ${lineHeight[typography]}px;
+  `;
+};
+
+export const setColor = (
+  defaultColor: string,
+  selectorStyleOptions: CSSSelectorOptions<"color"> = {}
+) => {
+  return css`
+    color: ${defaultColor};
+
+    ${getSelectorStyle("color", selectorStyleOptions)}
   `;
 };
