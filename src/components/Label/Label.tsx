9da9524f6709a4dca572be19d3018648a2597454
Haekang Jeon <1003gorkd@naver.com>
2023-04-18 17:35:15 +0900

    chore: props 직접 주입

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -2,9 +2,8 @@ import { LabelHTMLAttributes } from "react";
 import * as Styled from "./Label.styles";
 import { LabelProps } from "./Label.types";
 
-export const Label = ({
-  children,
-  ...props
-}: LabelProps & LabelHTMLAttributes<HTMLLabelElement>) => {
-  return <Styled.Label {...props}>{children}</Styled.Label>;
+export const Label = (
+  props: LabelProps & LabelHTMLAttributes<HTMLLabelElement>
+) => {
+  return <Styled.Label {...props} />;
 };
