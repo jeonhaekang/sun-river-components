7790552e53b936c232afc0b895422f98063595fb
jeonhaekang <1003gorkd@naver.com>
2023-04-29 14:19:55 +0900

    chore: default 값 지정

--- src/components/TextArea/TextArea.types.ts
+++ src/components/TextArea/TextArea.types.ts
@@ -4,7 +4,16 @@ import { STATUS } from "../Input";
 import { TEXTAREA_RESIZE } from "./TextArea.constants";
 
 export interface TextAreaProps {
+  /**
+   * @default none
+   */
   status?: OneOf<typeof STATUS>;
+  /**
+   * @default medium
+   */
   size?: SizeKey;
+  /**
+   * @default none
+   */
   resize?: OneOf<typeof TEXTAREA_RESIZE>;
 }
