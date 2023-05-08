4bac89b9516eebd690f9e93e15bf7457534e063d
jeonhaekang <1003gorkd@naver.com>
2023-05-08 11:06:39 +0900

    fix: remove export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useAutoPosition } from "./useAutoPosition";
 export { useBodyScrollLock } from "./useBodyScrollLock";
 export { useClickAway } from "./useClickAway";
-export { useHover } from "./useHover";
 export { useKeyPressEvent } from "./useKeyPressEvent";
